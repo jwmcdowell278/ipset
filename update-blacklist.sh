ba72bb64938f9b705f6e054f146c97dde941e641
jwmcdowell278 <jwmcdowell278@gmail.com>
2022-01-21 14:30:01 -0500

    Update update-blacklist.sh

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -7,15 +7,11 @@ IP_BLACKLIST_CUSTOM=/etc/ip-blacklist-custom.conf # optional
 list="chinese nigerian russian lacnic exploited-servers"
 BLACKLISTS=(
 "http://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1" # Project Honey Pot Directory of Dictionary Attacker IPs
-"http://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1"  # TOR Exit Nodes
 "http://www.maxmind.com/en/anonymous_proxies" # MaxMind GeoIP Anonymous Proxies
-"http://danger.rulez.sk/projects/bruteforceblocker/blist.php" # BruteForceBlocker IP List
 "http://rules.emergingthreats.net/blockrules/rbn-ips.txt" # Emerging Threats - Russian Business Networks List
 "http://www.spamhaus.org/drop/drop.lasso" # Spamhaus Don't Route Or Peer List (DROP)
 "http://cinsscore.com/list/ci-badguys.txt" # C.I. Army Malicious IP List
-"http://www.openbl.org/lists/base.txt"  # OpenBL.org 30 day List
 "http://www.autoshun.org/files/shunlist.csv" # Autoshun Shun List
-"http://lists.blocklist.de/lists/all.txt" # blocklist.de attackers
 )
 for i in "${BLACKLISTS[@]}"
 do
